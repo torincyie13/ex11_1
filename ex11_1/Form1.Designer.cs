c3ce7e8f97c1b77cd91eb41d697ad241d4b28bf6
Torin Hawkins <torincyie13@gmail.com>
2022-04-14 20:12:51 -0500

    Add project files.

---
+++ ex11_1/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace ex11_1
+{
+    partial class frmExport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(91, 35);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 0;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // frmExport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 86);
+            this.Controls.Add(this.btnExport);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmExport";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Export Data";
+            this.Load += new System.EventHandler(this.frmExport_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ ex11_1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace ex11_1
+{
+    public partial class frmExport : Form
+    {
+        SqlConnection booksConnection;
+        SqlCommand publishersCommand;
+        SqlDataAdapter publishersAdapter;
+        DataTable publishersTable;
+        public frmExport()
+        {
+            InitializeComponent();
+        }
+
+        private void frmExport_Load(object sender, EventArgs e)
+        {
+            string path = Path.GetFullPath("SQLBooksDB.mdf");
+            // connect to books database
+            booksConnection = new
+            //    SqlConnection("Data Source=.\\SQLEXPRESS; AttachDBFilename=" + Application.StartupPath + "\\SQLBooksDB.mdf;" +
+            //"Integrated Security=True; Connect Timeout=30; User Instance=True");
+            SqlConnection("Data Source=.\\SQLEXPRESS; AttachDBFilename=" + path + ";" +
+            "Integrated Security=True; Connect Timeout=30; User Instance=True;");
+            booksConnection.Open();
+            // establish command object
+            publishersCommand = new SqlCommand("Select * From Publishers", booksConnection);
+            // establish data adapter/data table
+            publishersAdapter = new SqlDataAdapter();
+            publishersAdapter.SelectCommand = publishersCommand;
+            publishersTable = new DataTable();
+            publishersAdapter.Fill(publishersTable);
+        }
+
+        private void frmExport_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // close the connection KWSalesConnection.Close(); // dispose of theobjects
+            booksConnection.Dispose();
+            publishersCommand.Dispose(); 
+            publishersAdapter.Dispose();
+            publishersTable.Dispose();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            StreamWriter outputFile; 
+            outputFile = new StreamWriter(Application.StartupPath + "\\publishers.csv"); 
+            // write headers
+            for (int n = 0; n < publishersTable.Columns.Count; n++)
+            {
+                outputFile.Write(publishersTable.Columns[n]); 
+                if (n < publishersTable.Columns.Count - 1) 
+                    outputFile.Write(",");
+            }
+            outputFile.Write(outputFile.NewLine); 
+            // write all fields
+            foreach(DataRow myRow in publishersTable.Rows) 
+            {
+                for (int n = 0; n < publishersTable.Columns.Count; n++)
+                {
+                    if (myRow[n] != null) outputFile.Write(myRow[n].ToString());
+                    if (n < publishersTable.Columns.Count - 1)
+                        outputFile.Write(",");
+                }
+                outputFile.Write(outputFile.NewLine);
+            }
+            outputFile.Close();
+        }
+    }
+}
